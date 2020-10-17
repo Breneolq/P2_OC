0c118300f39aac877b75d7407abfe8725e17f7a7
Djuns35 <q.breneol@gmail.com>
2020-10-17 15:29:17 +0200

    Mise en place des étapes a réaliser avant livrable final

---
+++ main.py
@@ -0,0 +1,48 @@
+        #Première Etape#
+
+"""
+    -Choisir une page Produit
+
+"""
+
+url_produit = 'http://books.toscrape.com/catalogue/in-a-dark-dark-wood_963/index.html'
+
+"""
+    
+    -Extraire les infos:
+        product_page_url
+        universal_product_code
+        title
+        price_including_tax
+        price_excluding_tax
+        number_available
+        product_description
+        category
+        review_rating
+        image_url
+    
+    -Ecrire les données dans un fichier CSV en colonne avec
+    les valeurs ci dessus comme en-tête
+"""
+
+        #Seconde Etape#
+
+"""
+    -Etendre le script afin de récupérer toutes les données d'une
+    catégorie
+
+    -Attention au passage d'une page à l'autre
+"""
+
+        #Troisième Etape#
+
+"""
+    -Parcourir toutes les catégories et récupérer les infos
+    de tous les produits dans autant de fichiers CSV qu'il
+    y a de catégories
+"""
+
+        #Quatrième Etape#
+"""
+    Télécharger et enregistrer le fichier image de chaque livre
+"""
