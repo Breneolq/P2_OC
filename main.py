99ad6d46bff1a71f8428152e47fb683360828185
Djuns35 <q.breneol@gmail.com>
2020-10-19 04:15:01 +0200

    Identification des variables

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
         #Première Etape#
 
 """
-    -Choisir une page Produit
+    -Choisir une page Produit & se connecter
 
 """
 
 url_produit = 'http://books.toscrape.com/catalogue/in-a-dark-dark-wood_963/index.html'
+response = requests.get(url_produit)
 
 """
     
@@ -21,6 +26,24 @@ url_produit = 'http://books.toscrape.com/catalogue/in-a-dark-dark-wood_963/index
         review_rating
         image_url
     
+"""
+if response.ok:
+    soup = BeautifulSoup(response.text, features="html.parser")
+    product_page_url = url_produit
+    universal_product_code = 0
+    title = 0
+    price_including_tax = 0
+    price_excluding_tax = 0
+    number_available = 0
+    product_description = 0
+    category = 0
+    review_rating = 0
+    image_url = 0
+
+    print(product_page_url)
+
+
+"""
     -Ecrire les données dans un fichier CSV en colonne avec
     les valeurs ci dessus comme en-tête
 """
