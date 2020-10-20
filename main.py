5e15b0d90fce184de3bbea1cb8ebf68407d45d23
Breneol <quentin.breneol@sas-automotive.com>
2020-10-20 18:25:51 +0200

    Refactorisation de la partie scrapping du produit

--- main.py
+++ main.py
@@ -4,74 +4,56 @@ from bs4 import BeautifulSoup
 import urllib.parse
 
 url_home = 'http://books.toscrape.com/'
-url_produit = 'http://books.toscrape.com/catalogue/in-a-dark-dark-wood_963/index.html'
-response = requests.get(url_produit)
-response.encoding = 'utf-8'
 
-
-if response.ok:
-    soupe = BeautifulSoup(response.text, features="html.parser")
-    product_page_url = url_produit
+def GetBookInfos(url_product):
     
-def SearchInBoard(id, soup):
-    for i in soup.find_all("th"):
-        if i.text.strip() == id:
-            return(i.find_next("td").text.strip())
+    response = requests.get(url_product)
+    response.encoding = 'utf-8'
 
-def SearchDescription(soup):
-    meta = soup.find('meta', {'name': 'description'})
-    description = meta['content']
-    return(description)
 
-def SearchTitle(soup):
-    return(soup.find('div', {'class': 'col-sm-6 product_main'}).find('h1').text.strip())
+    if response.ok:
+        soupe = BeautifulSoup(response.text, features="html.parser")
+    
+    def SearchInBoard(id, soup):
+        for i in soup.find_all("th"):
+            if i.text.strip() == id:
+                return(i.find_next("td").text.strip())
 
-def SearchCategory(soup):
-    category = soup.find('ul', {'class': 'breadcrumb'}).find_all(['a'])
-    return(category[2].text.strip())
+    def SearchDescription(soup):
+        meta = soup.find('meta', {'name': 'description'})
+        description = meta['content']
+        return(description)
 
-def SearchImageUrl(soup, url):
-    img = soup.find('div', {'class': 'thumbnail'}).find('div', {'class': 'carousel-inner'}).find('div', {'class': 'item active'}).find('img')
-    link = img['src']
-    return urllib.parse.urljoin(url, link)
+    def SearchTitle(soup):
+        return(soup.find('div', {'class': 'col-sm-6 product_main'}).find('h1').text.strip())
 
-def WriteInCSV(title, category, product_description, universal_product_code, price_including_tax, price_excluding_tax, number_available, review_rating, image_url):
-    with open(category +'.csv', mode='w', newline='') as test_file:
-        test_writer = csv.writer(test_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        test_writer.writerow(['title', 'category', 'product_description', 'universal_product_code', 'price_including_tax', 'price_excluding_tax', 'number_available', 'review_rating', 'image_url'])
-        test_writer.writerow([title, category, product_description, universal_product_code, price_including_tax, price_excluding_tax, number_available, review_rating, image_url])
+    def SearchCategory(soup):
+        category = soup.find('ul', {'class': 'breadcrumb'}).find_all(['a'])
+        return(category[2].text.strip())
 
-universal_product_code = SearchInBoard("UPC", soupe)
-price_including_tax = SearchInBoard("Price (incl. tax)", soupe)
-price_excluding_tax = SearchInBoard("Price (excl. tax)", soupe)
-number_available = SearchInBoard("Availability", soupe)
-review_rating = SearchInBoard("Number of reviews", soupe)
-product_description = SearchDescription(soupe)
-title = SearchTitle(soupe)
-category = SearchCategory(soupe)
+    def SearchImageUrl(soup, url):
+        img = soup.find('div', {'class': 'thumbnail'}).find('div', {'class': 'carousel-inner'}).find('div', {'class': 'item active'}).find('img')
+        link = img['src']
+        return urllib.parse.urljoin(url, link)
 
-image_url = SearchImageUrl(soupe, url_home)
+    def WriteInCSV(title, category, product_description, universal_product_code, price_including_tax, price_excluding_tax, number_available, review_rating, image_url):
+        with open(category +'.csv', mode='w', newline='') as test_file:
+            test_writer = csv.writer(test_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            test_writer.writerow(['title', 'category', 'product_description', 'universal_product_code', 'price_including_tax', 'price_excluding_tax', 'number_available', 'review_rating', 'image_url'])
+            test_writer.writerow([title, category, product_description, universal_product_code, price_including_tax, price_excluding_tax, number_available, review_rating, image_url])
 
-WriteInCSV(title, category, product_description, universal_product_code, price_including_tax, price_excluding_tax, number_available, review_rating, image_url)
+    universal_product_code = SearchInBoard("UPC", soupe)
+    price_including_tax = SearchInBoard("Price (incl. tax)", soupe)
+    price_excluding_tax = SearchInBoard("Price (excl. tax)", soupe)
+    number_available = SearchInBoard("Availability", soupe)
+    review_rating = SearchInBoard("Number of reviews", soupe)
+    product_description = SearchDescription(soupe)
+    title = SearchTitle(soupe)
+    category = SearchCategory(soupe)
 
-        #Seconde Etape#
+    image_url = SearchImageUrl(soupe, url_home)
 
-"""
-    -Etendre le script afin de récupérer toutes les données d'une
-    catégorie
+    WriteInCSV(title, category, product_description, universal_product_code, price_including_tax, price_excluding_tax, number_available, review_rating, image_url)
 
-    -Attention au passage d'une page à l'autre
-"""
-
-        #Troisième Etape#
-
-"""
-    -Parcourir toutes les catégories et récupérer les infos
-    de tous les produits dans autant de fichiers CSV qu'il
-    y a de catégories
-"""
-
-        #Quatrième Etape#
-"""
-    Télécharger et enregistrer le fichier image de chaque livre
-"""
+Test_url = 'http://books.toscrape.com/catalogue/this-one-summer_947/index.html'
+GetBookInfos(Test_url)
