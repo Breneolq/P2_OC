80a54fe74683a8dc2159e3d4ed9985580298b6ad
Djuns35 <q.breneol@gmail.com>
2020-10-30 11:26:02 +0100

    Début de refonte du code en POO

--- src/browser.py
+++ src/browser.py
@@ -1,4 +1,14 @@
+from bs4 import BeautifulSoup
+
 class Browser:
 
-    def __init__(self):
+    def __init__(self, parsed_page):
+        self.parsed_page = parsed_page
+        
+    def browse_categories(self):
+        a = self.parsed_page.find('div', {'class': 'side_categories'}).find('ul', {'class':'nav nav-list'}).find('li').find('ul').find('li').find('a')
+        category_url = 'http://books.toscrape.com/' + a['href']
+        print(category_url)
+    
+    def browse_books(self):
         pass
--- src/request.py
+++ src/request.py
@@ -1,4 +1,5 @@
 import requests
+from parse import Parser
 
 class Request:
 
@@ -6,4 +7,13 @@ class Request:
         self.url = url
 
     def request_url(self):
-        pass
+            response = requests.get(self.url)
+            if response.ok:
+                parsing_response = Parser(response)
+                parsing_response.html_parser()
+        #except:
+            #print("Nous n'avons pas réussi à établir une connexion avec la page demandée.")
+
+url = 'http://books.toscrape.com/'
+request = Request(url)
+request.request_url()
