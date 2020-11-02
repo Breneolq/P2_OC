9d8a7c3c01993967789245c7676a39c1f0b647ca
Djuns35 <q.breneol@gmail.com>
2020-11-02 19:00:08 +0100

    Récupération des livres d'une page

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from booktoscrape.parse import Parser
 from booktoscrape.scrapper import Scrapper
 
@@ -11,7 +12,19 @@ parsed_html = parse_response.html_parser()
 scrapper = Scrapper(parsed_html)
 category_list = scrapper.scrap_categories()
 
-print(category_list)
+i = 0
+
+while i < len(category_list):
+    category_response = Parser(category_list[i])
+    i += 1
+    category_html = category_response.html_parser()
+
+    category_scrapper = Scrapper(category_html)
+
+    book_list = category_scrapper.scrap_books()
+
+    print(book_list)
+
 
 
 
