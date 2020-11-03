b3664c4e32e5db9e49720abf327a8638c0efb002
Djuns35 <q.breneol@gmail.com>
2020-11-03 14:28:10 +0100

    Mise en place de 3 appeles séparés afin de pourvoir récupérer les catégories et les livres de la première page

--- __main__.py
+++ __main__.py
@@ -6,21 +6,31 @@ if __name__ == "__main__":
     
     url = 'http://books.toscrape.com/'
 
-parse_response = Parser(url)
-parsed_html = parse_response.html_parser()
+#Test méthode de récupération URL des catégories
 
-scrapper = Scrapper(parsed_html)
+parse_response = Parser(url)
+parsed_response = parse_response.html_parser()
+
+scrapper = Scrapper(parsed_response)
 list_category = scrapper.scrap_categories()
 
-#Test méthode de récupération URL de page
-
+#Test méthode de récupération URL de la page suivante
+"""
 category_response = Parser(list_category[3])
 category_html = category_response.html_parser()
 
 category_scrapper = Scrapper(category_html)
 category_url_next_page = category_scrapper.scrap_page_in_categories(list_category[3])
+"""
+#Test méthode de récupération Url des livres sur la page
+"""
+book_response = Parser(list_category[0])
+book_response_parsed = book_response.html_parser()
 
-print(category_url_next_page)
+book_url_scrapper = Scrapper(book_response_parsed)
+book_url_list = book_url_scrapper.scrap_books()
+"""
+print(list_category)
 
 
 
