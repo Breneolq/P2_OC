b6f736396d5f437a1b01397fe3e1f5f5dd839032
Djuns35 <q.breneol@gmail.com>
2020-11-09 11:52:46 +0100

    Modification de certaines fonctions et récupération des données des livres.

--- __main__.py
+++ __main__.py
@@ -5,14 +5,15 @@ from booktoscrape.scrapper import Scrapper
 def main ():
 
     URL = 'http://books.toscrape.com/'
+    BOOKS_IDS = ['UPC', 'Price (incl. tax)', 'Price (excl. tax)', 'Availability', 'Number of reviews']
 
     parser = Parser()
-    scrapper = Scrapper()
+    scrapper = Scrapper(parser, URL, BOOKS_IDS)
 
     parsed_response = parser.html_parser(URL)
     category_list = scrapper.scrap_categories(parsed_response)
 
-    scrapper.scrap_books_in_category(category_list, parser, scrapper)
+    scrapper.scrap_books_in_category(category_list, scrapper)
 
 if __name__ == "__main__":
 
--- booktoscrape/scrapper.py
+++ booktoscrape/scrapper.py
@@ -3,18 +3,20 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 class Scrapper:
-    
-    url_list = []
  
-    def __init__(self):
-        pass
+    def __init__(self, parser, url, list_of_books_ids):
+        self.__parser = parser
+        self.base_url = url
+        self.books_ids_search = list_of_books_ids
+
+    def create_soup(self, url):
+        return self.__parser.html_parser(url)
         
     def scrap_categories(self, parsed_page):
         category_list = []
         tab_of_li = parsed_page.find('ul', {'class': 'nav nav-list'}).find('li').find('ul').find_all('li')
         for li in tab_of_li:
-            a = li.find('a')
-            link = a['href']
+            link = li.find('a')['href']
             category_url = 'http://books.toscrape.com/' + link
             category_list.append(category_url)
         return category_list
@@ -22,32 +24,65 @@ class Scrapper:
     def scrap_books(self, parsed_page):
         list_book = []
         tab_of_li = parsed_page.find_all('li', {'class':'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+        start_link = 'http://books.toscrape.com/catalogue/'
         for li in tab_of_li:
-            a = li.find('article', {'class':'product_pod'}).find('div', {'class':'image_container'}).find('a')
-            end_link = a['href']
+            end_link = li.find('article', {'class':'product_pod'}).find('div', {'class':'image_container'}).find('a')['href']
             end_link_short = end_link[9:]
-            start_link = 'http://books.toscrape.com/catalogue/'
             link = urljoin(start_link, end_link_short)
             list_book.append(link)
        
         return list_book
     
-    def scrap_books_in_category(self, category_list, parser, scrapper):
+    def scrap_books_in_category(self, category_list, scrapper):
         i = 0
         while i < len(category_list):
 
-            category_html = parser.html_parser(category_list[i])
+            category_html = scrapper.create_soup(category_list[i])
             book_in_page_1 = scrapper.scrap_books(category_html)
 
             while category_html.find('li', {'class':'next'}):
-                a = category_html.find('li', {'class': 'next'}).find('a')
-                next_page = a['href']
+                next_page = category_html.find('li', {'class': 'next'}).find('a')['href']
                 category_url_next_page = urljoin(category_list[i], next_page)
-                next_page_html = parser.html_parser(category_url_next_page)
+                next_page_html = scrapper.create_soup(category_url_next_page)
                 books_in_page = scrapper.scrap_books(next_page_html)
                 book_in_page_1 = book_in_page_1 + books_in_page
                 category_html = next_page_html
+            print(i)
+            scrapper.books_url(book_in_page_1, scrapper)
             i += 1
+    
+    def books_url(self, book_list, scrapper):
+        for book in book_list:
+            book_html = scrapper.create_soup(book)
+            title = scrapper.scrap_book_title(book_html)
+            category = scrapper.scrap_book_category(book_html)
+            description = scrapper.scrap_book_description(book_html)
+            image_url = scrapper.scrap_book_image_url(book_html)
+            print(title, category, description, image_url)
+
+    
+    def scrap_book_title(self, soup):
+        return(soup.find('div', {'class': 'col-sm-6 product_main'}).find('h1').text.strip())
+    
+    def scrap_in_board(self, soup):
+        for id in self.books_ids_search:
+            for i in soup.find_all("th"):
+                if i.text.strip() == id:
+                    return(i.find_next("td").text.strip())
+
+    def scrap_book_description(self, soup):
+        meta = soup.find('meta', {'name': 'description'})
+        description = meta['content']
+        return(description)
+
+    def scrap_book_category(self, soup):
+        category = soup.find('ul', {'class': 'breadcrumb'}).find_all(['a'])
+        return(category[2].text.strip())
+
+    def scrap_book_image_url(self, soup):
+        link = soup.find('div', {'class': 'thumbnail'}).find('div', {'class': 'carousel-inner'}).find('div', {'class': 'item active'}).find('img')['src']
+        return(urljoin(self.base_url, link))
+
         
         
     
